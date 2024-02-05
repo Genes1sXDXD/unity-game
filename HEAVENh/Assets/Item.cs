3f8206ac1f5458b6b22d0e13c68a01abd764c9fc
xXminimannenXx <anton.finn@elev.ga.klaragymnasium.se>
2024-02-05 10:57:38 +0100

    preAlpha0.2
    inventory uppdate

---
+++ HEAVENh/Assets/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item : MonoBehaviour
+{
+    public int id;
+    public string nameItem;
+    // Add more properties as needed
+}
+
+
