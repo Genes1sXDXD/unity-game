d1b7e9860fa8618fed51f5deeb6298bdcd8337c2
W1LLUMP <silverspelaren@gmail.com>
2024-03-18 09:42:06 +0100

    vhgfvghjh

--- HEAVENh/Assets/Inventory.cs
+++ HEAVENh/Assets/Inventory.cs
@@ -1,36 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public List<Item> items = new List<Item>();
-    public int inventorySize = 20;
-
-    public bool AddItem(Item item)
+    // Start is called before the first frame update
+    void Start()
     {
-        if (items.Count < inventorySize)
-        {
-            items.Add(item);
-            // Update UI or perform other actions
-            return true;
-        }
-        else
-        {
-            // Inventory full, handle accordingly
-            return false;
-        }
+        
     }
 
-    public void RemoveItem(Item item)
+    // Update is called once per frame
+    void Update()
     {
-        items.Remove(item);
-        // Update UI or perform other actions
-    }
-
-    public bool HasItem(Item item)
-    {
-        return items.Contains(item);
+        
     }
 }
